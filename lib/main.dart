79e1ea0205df487857aac3f9d24428f05a559962
nguyenducmycanvas <nguyenducmy.canvas@gmail.com>
2021-08-05 23:00:16 +0700

    Add first layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,58 @@
-// Bước 1: import thư viện material vào, thư viện này sẽ cung cấp widget để code
 import 'package:flutter/material.dart';
 
-// Bước 2: khai báo hàm main, đây là nơi mà code sẽ thực thi đầu tiên
 void main() {
-  // Bước 3: gọi hàm runApp truyền vào 1 object MaterialApp
-  // MaterialApp chính là widget root, tổ tiên của 1 cây widget sau này
-  runApp(MaterialApp(
-      // Bước 4: Trong constructor của MaterialApp có 1 property là `home`
-      // ta sử dụng property `home` này để code nội dung trong app
-      // ở đây mình truyền vào widget Text truyền vào 1 String
-      home: Text('Hi bạn, cho mình làm quen nh!')));
+  runApp(
+    MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.pink,
+            title: Text('Cua nang Flutter'),
+          ),
+          body: Column(
+            children: [
+              Text('Hi ban, cho minh lam quen nhe'),
+              Row(
+                children: [
+                  TextButton(
+                    child: Text('Red Btn'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.white,
+                      onSurface: Colors.red,
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: () {
+                      print('Pressed');
+                    },
+                  ), //Button Red
+                  TextButton(
+                    child: Text('Yellow btn'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      onSurface: Colors.yellow,
+                      backgroundColor: Colors.yellow,
+                    ),
+                    onPressed: () {
+                      print('Pressed');
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Green btn'),
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      onSurface: Colors.red,
+                      backgroundColor: Colors.green,
+                    ),
+                    onPressed: () {
+                      print('Pressed');
+                    },
+                  ) // Button Green
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
