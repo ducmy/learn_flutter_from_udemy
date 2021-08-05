56586f8b871b56397fa234eb725be81e822bcd73
nguyenducmycanvas <nguyenducmy.canvas@gmail.com>
2021-08-05 23:17:57 +0700

    Change to text

--- lib/main.dart
+++ lib/main.dart
@@ -9,50 +9,65 @@ void main() {
             backgroundColor: Colors.pink,
             title: Text('Cua nang Flutter'),
           ),
-          body: Column(
-            children: [
-              Text('Hi ban, cho minh lam quen nhe'),
-              Row(
-                children: [
-                  TextButton(
-                    child: Text('Red Btn'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      onSurface: Colors.red,
-                      backgroundColor: Colors.red,
-                    ),
-                    onPressed: () {
-                      print('Pressed');
-                    },
-                  ), //Button Red
-                  TextButton(
-                    child: Text('Yellow btn'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.black,
-                      onSurface: Colors.yellow,
-                      backgroundColor: Colors.yellow,
-                    ),
-                    onPressed: () {
-                      print('Pressed');
-                    },
-                  ),
-                  TextButton(
-                    child: Text('Green btn'),
-                    style: TextButton.styleFrom(
-                      primary: Colors.black,
-                      onSurface: Colors.red,
-                      backgroundColor: Colors.green,
-                    ),
-                    onPressed: () {
-                      print('Pressed');
-                    },
-                  ) // Button Green
-                ],
-              )
-            ],
-          ),
+          body: buildColumn(),
         ),
       ),
     ),
   );
+}
+
+buildColumn() {
+  return ColumnWidget(); // refector from here
+}
+
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Bay gio thi hot dc roi nha! Hot reload duoc roi day'),
+        Row(
+          children: [
+            TextButton(
+              child: Text('Red Btn'),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                onSurface: Colors.red,
+                backgroundColor: Colors.red,
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            ), //Button Red
+            TextButton(
+              child: Text('Yellow btn'),
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+                onSurface: Colors.yellow,
+                backgroundColor: Colors.yellow,
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            ),
+            TextButton(
+              child: Text('Green btn'),
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+                onSurface: Colors.red,
+                backgroundColor: Colors.green,
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+            ) // Button Green
+          ],
+        )
+      ],
+    );
+  }
 }
