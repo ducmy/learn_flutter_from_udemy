5cbd5b02e9a2c7ac11be11661720d7623962b879
nguyenducmycanvas <nguyenducmy.canvas@gmail.com>
2021-08-07 09:19:58 +0700

    Update haft

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,47 +11,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Sử dụng data thi dung StatefulWidget
-// StatefulWidget con duoc dung de rebuild subWidget
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int counter = 0; // Data cua Widget
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: MyText(counter: counter),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: Icon(Icons.add),
+        child: ElevatedButton(
+          onPressed: () {
+            final snackBar = SnackBar(content: Text('This is snack Bar ^^'));
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+          child: const Text('Show SnackBar'),
+        ),
       ),
     );
-  }
-}
-
-// Hiển thị data thì dùng StatelessWidget
-class MyText extends StatelessWidget {
-  const MyText({
-    Key? key,
-    required this.counter,
-  }) : super(key: key);
-
-  final int counter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Tui la Widget Text. Data cua tui la: $counter ');
   }
 }
